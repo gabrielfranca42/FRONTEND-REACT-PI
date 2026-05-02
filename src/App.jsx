4250d39332c49e5ab900985020a3fb8c3dc94ec6
gabrielfranca42 <gabrielfranca172@gmail.com>
2026-05-01 21:49:44 -0300

    tentando jogar pra prod

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import AdminLayout from './layouts/AdminLayout';
 import CoordinatorLayout from './layouts/CoordinatorLayout';
 import Login from './pages/Login';
